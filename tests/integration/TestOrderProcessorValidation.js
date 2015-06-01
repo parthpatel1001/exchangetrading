9a6c7dda66667846a3090c09afb5a612fcc457bc
Parth Patel <parthpatel1001@users.noreply.github.com>
2015-06-01 19:18:26 -0400

    add test directories....TOTALLY NOT FINISHED TESTING WANT FOOD

---
+++ tests/integration/TestOrderProcessorValidation.js
@@ -0,0 +1,16 @@
+/**
+ * Created by parthpatel1001 on 6/1/15.
+ */
+var simple = require('simple-mock');
+var ExchangeManager = {};
+var BalanceTracker = {};
+
+var OrderProcessor = require('../../lib/Order/OrderProcessor.js');
+
+var assert = require("assert");
+
+describe('OrderProcessor', function(){
+    it('Should check the balance before processing a linked order',function(done){
+
+    });
+});
