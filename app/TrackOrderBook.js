64d99891ee650dc174111b054f621145eb061d76
Mike <lipsitzm@gmail.com>
2015-06-05 12:21:07 -0600

    moving OrderBookTracker creation from the app into the subscriber

--- app/TrackOrderBook.js
+++ app/TrackOrderBook.js
@@ -1,7 +1,4 @@
 var
-    OrderBookSubscriber = require('../lib/OrderBook/OrderBookSubscriber.js'),
-    OrderBookTracker = require('../lib/OrderBook/OrderBookTracker.js');
+    OrderBookSubscriber = require('../lib/OrderBook/OrderBookSubscriber.js');
 
-OrderBookTracker = new OrderBookTracker();
 OrderBookSubscriber = new OrderBookSubscriber();
-OrderBookSubscriber.subscribeToOrderBookTop(OrderBookTracker.trackOrderBook);
