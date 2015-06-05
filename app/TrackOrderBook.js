9e9583cb6d74aeff512856273a21ed11d918bd49
Mike <lipsitzm@gmail.com>
2015-06-05 12:51:49 -0600

    orderBookSubscriber needs to have a subscribe method because there are 2 consumers of it

--- app/TrackOrderBook.js
+++ app/TrackOrderBook.js
@@ -1,4 +1,7 @@
 var
-    OrderBookSubscriber = require('../lib/OrderBook/OrderBookSubscriber.js');
+    OrderBookSubscriber = require('../lib/OrderBook/OrderBookSubscriber.js'),
+    OrderBookTracker = require('../lib/OrderBook/OrderBookTracker.js');
 
+OrderBookTracker = new OrderBookTracker();
 OrderBookSubscriber = new OrderBookSubscriber();
+OrderBookSubscriber.subscribeToOrderBookTop(OrderBookTracker.trackOrderBook);
