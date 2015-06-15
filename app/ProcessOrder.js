d872bc345f99b260ad72abad93efea72564c9e33
Mike <lipsitzm@gmail.com>
2015-06-14 19:56:39 -0600

    cleanup of processOrder / orderProcessor that was missed

--- app/ProcessOrder.js
+++ app/ProcessOrder.js
@@ -3,14 +3,10 @@ var OrderSubscriber = require('../lib/Order/OrderSubscriber.js'),
     CoinbaseExchange  = require('../lib/Exchange/Coinbase/CoinbaseExchange.js'),
     BitstampExchange  = require('../lib/Exchange/Bitstamp/BitstampExchange.js'),
     ExchangeManager   = require('../lib/Exchange/ExchangeManager.js'),
-    BalanceTracker = require('../lib/Balance/BalanceTracker.js'),
     config = require('config'),
-    Notification = require('../lib/Notification.js'), // TODO MOVE THIS TO A NAMESPACE/DOMAIN FOLDER;
+    Notification = require('../lib/Notification.js'), // TODO MOVE THIS TO A NAMESPACE/DOMAIN FOLDER
     async = require('async'),
     pm2 = require('pm2');
-
-var notifier = new Notification();
-var opts = config.get('Notification.Slack.error_config');
 
 var coinbase = new CoinbaseExchange(),
     bitstamp = new BitstampExchange();
@@ -22,18 +18,16 @@ for(var i = 0, len = exchanges.length; i < len; i++) {
     ExchangeManager.addExchange(exchanges[i]);
 }
 
-BalanceTracker = new BalanceTracker();
-
-OrderProcessor = new OrderProcessor(ExchangeManager,BalanceTracker,notifier);
+OrderProcessor = new OrderProcessor(ExchangeManager);
 OrderSubscriber = new OrderSubscriber();
 
 OrderSubscriber.subscribeToLinkedOrderStream(config.get('EventChannels.LINKED_ORDER_STREAM'),OrderProcessor.processLinkedOrder);
 
-
-
 process.on('uncaughtException',function(e){
     console.error('Uncaught Exception',e);
 
+    var notifier = new Notification(),
+        opts = config.get('Notification.Slack.error_config');
     var error = e.toString() || JSON.stringify(e);
 
     async.parallel([
