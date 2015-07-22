d2abb4cc0e41867427c8355d002f0451174e1e72
Mike <lipsitzm@gmail.com>
2015-07-22 14:04:20 -0600

    babel can now be found by module importer (but some libs aren't mapped right yet), serializer test now waits on async stuff

--- test/Utilities/SerializerTests.js
+++ test/Utilities/SerializerTests.js
@@ -4,18 +4,21 @@ import {BuyOrder} from '../../lib/Order/BuyOrder';
 import {SerializeObject} from '../../lib/Utilities/Serializer';
 
 describe('Serializer', function(){
-    it('Should return a string version of the passed in very simple object',function(){
+    it('Should return a string version of the passed in very simple object',function(done){
         console.log('starting serialize test');
         let price = 3,
             amount = 5,
             order = new BuyOrder(null, price, amount, null);
 
-        SerializeObject(order, './lib/Order/BuyOrder').then((serialized) => {
+        SerializeObject(order, './lib/Order/BuyOrder.js').then((serialized) => {
             console.log('SerializeObject success', serialized);
 
             assert(serialized instanceof String);
+            done();
         }).catch((err) => {
             console.log('SerializeObject err', err);
+            assert(false);
+            done();
         });
     });
 });
