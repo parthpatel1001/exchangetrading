38da876b0e1727ae3650fc8a97d3645ab4a86dd8
Mike <lipsitzm@gmail.com>
2015-08-06 23:21:29 -0600

    fixing TestOrderGenerator

--- test/Order/TestOrderGenerator.js
+++ test/Order/TestOrderGenerator.js
@@ -12,19 +12,24 @@ import config from 'config';
 describe('OrderGenerator', function(){
     // set these up in global scope
     let clientMock = null;
-    var orderSub,orderPub,orderGen,buyPrice,sellPrice,amount,exchangeIds,safetyAmount;
+    var orderSub,orderPub,orderGen,buyPrice,sellPrice,amount,exchanges, publisherCalled;
     beforeEach(function() {
         clientMock = redisMock.createClient();
         RedisWrapper.setClients(clientMock, clientMock);
         orderSub = new OrderSubscriber();
-        orderPub = new OrderPublisher();
+        publisherCalled = false;
+        orderPub = {
+            publishLinkedOrders: () => {
+                publisherCalled = true;
+            }
+        };
         orderGen = new OrderGenerator(orderPub);
 
         //dummy data to be used for tests
         buyPrice = num(9);
         sellPrice = num(7);
         amount = num(13);
-        exchangeIds = [0,1];
+        exchanges = [{exchangeId: 0},{exchangeId: 1}];
     });
 
     afterEach(function() {
@@ -34,12 +39,14 @@ describe('OrderGenerator', function(){
     it("Asserts that we do not buy more than the safety value", function(){
         var callback = function(order1, order2) {
             expect(order1.Amount.cmp(amount)).to.equal(-1); //less than amount specified
-            expect(order1.Amount.cmp(safetyAmount)).to.equal(0); //same as safetyAmount
+            expect(order1.Amount.sub(1) < order1.Amount).to.equal(true); //within the margin of the safety amount
+            expect(order1.Amount.add(1) > order1.Amount).to.equal(true); //within the margin of the safety amount
+            expect(publisherCalled).to.be(true);
         };
 
         var CHANNEL = config.get('EventChannels.LINKED_ORDER_STREAM');
 
         orderSub.subscribeToLinkedOrderStream(CHANNEL, callback);
-        orderGen.generateOrder(exchangeIds, buyPrice, sellPrice, amount);
+        orderGen.generateOrder(exchanges, buyPrice, sellPrice, amount);
     });
 });
